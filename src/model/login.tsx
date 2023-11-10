4264edacb085397fb1643455a6f10a85647a7179
d33d4y0 <d33d4y@gmail.com>
2023-11-10 16:01:22 +0700

    change to pure reactjs

--- src/model/login.tsx
+++
@@ -1,5 +0,0 @@
-interface Login {
-    user: string;
-    accessToken: string;
-    refreshToken: string;
-}
--- src/router/appRouter.ts
+++
@@ -1,8 +0,0 @@
-import { useRouter } from "next/navigation";
-
-const useAppRouter = () => {
-    const router = useRouter();
-    return router;
-}
-
-export default useAppRouter
