43b62e26cd1755387db953f524a8823718e12390
d33d4y0 <d33d4y@gmail.com>
2023-11-21 19:32:17 +0700

    add style to NavBar

--- src/component/NavBar/index.tsx
+++ src/component/NavBar/index.tsx
@@ -1,63 +1,66 @@
-import { MdOutlinePerson } from "react-icons/md";
+import {
+  MdOutlinePerson,
+  MdOutlineSearch,
+  MdOutlineShoppingBag,
+} from "react-icons/md";
 import { RootState } from "@/redux/store";
 import { useSelector } from "react-redux";
-import { useNavigate } from "react-router-dom";
+import { useNavigate, useLocation, NavLink } from "react-router-dom";
 
 const NavBar = () => {
-    const isLoggedIn = useSelector((state: RootState) => state.auth.isLoggedIn);
-    const navigate = useNavigate();
+  const isLoggedIn = useSelector((state: RootState) => state.auth.isLoggedIn);
+  const navigate = useNavigate();
+  const location = useLocation();
 
-    const handleOnClickProfile = () => {
-        if (isLoggedIn) {
-            // TODO show list of link to account page, logout, etc
-            navigate("/account");
-        } else {
-            navigate("/login");
-            //  TODO show list of link to login, register, etc
-        }
+  const handleOnClickProfile = () => {
+    if (isLoggedIn) {
+      // TODO show list of link to account page, logout, etc
+      navigate("/account");
+    } else {
+      navigate("/login");
+      //  TODO show list of link to login, register, etc
     }
+  };
 
-    return (
-        <div className="w-[1440px] h-[60px] px-40 py-4 bg-white justify-between items-center inline-flex">
-            <div className="justify-center items-center flex">
-                <div className="text-center"><span className="text-black text-2xl font-medium font-['Poppins'] leading-normal">3legant</span><span className="text-zinc-500 text-2xl font-medium font-['Poppins'] leading-normal">.
-                </span></div>
-            </div>
-            <div className="justify-center items-center gap-10 flex">
-                <div className="justify-start items-center gap-0.5 flex">
-                    <div className="border-neutral-900 justify-start items-center gap-0.5 flex">
-                        <div className="justify-start items-center gap-1 flex">
-                            <button className="text-neutral-900 text-sm font-medium font-['Space Grotesk'] leading-normal" onClick={() => navigate("/")}>Home</button>
-                        </div>
-                    </div>
-                </div>
-                <div className="justify-start items-center gap-0.5 flex">
-                    <div className="border-zinc-500 justify-start items-center gap-0.5 flex">
-                        <div className="justify-start items-center gap-1 flex">
-                            <button className="text-zinc-500 text-sm font-medium font-['Space Grotesk'] leading-normal">Shop</button>
-                        </div>
-                    </div>
-                </div>
-                <div className="justify-start items-center gap-0.5 flex">
-                    <div className="border-zinc-500 justify-start items-center gap-0.5 flex">
-                        <div className="justify-start items-center gap-1 flex">
-                            <button className="text-zinc-500 text-sm font-medium font-['Space Grotesk'] leading-normal">Product</button>
-                        </div>
-                    </div>
-                </div>
-                <div className="justify-start items-center gap-0.5 flex">
-                    <div className="border-zinc-500 justify-start items-center gap-0.5 flex">
-                        <div className="justify-start items-center gap-1 flex">
-                            <button className="text-zinc-500 text-sm font-medium font-['Space Grotesk'] leading-normal">Contact Us</button>
-                        </div>
-                    </div>
-                </div>
-            </div>
-            <button className="w-5 h-5 relative flex-col justify-start items-start flex" onClick={handleOnClickProfile}>
-                <MdOutlinePerson />
-            </button>
+  const currentPathCondition = (path: string) => {
+    return location.pathname === path
+      ? "text-sm font-medium text-neutral-900"
+      : "text-sm font-medium text-zinc-500";
+  };
+  return (
+    <>
+      <div className="container flex  w-full max-w-none items-center justify-between px-40 py-4">
+        <div>
+          <span className="text-2xl font-medium text-black">3legant.</span>
         </div>
-    )
-}
+        <div className="flex items-center justify-center gap-20">
+          <NavLink to="/" className={currentPathCondition("/")}>
+            Home
+          </NavLink>
+          <NavLink to="/shop" className={currentPathCondition("/shop")}>
+            Shop
+          </NavLink>
+          <NavLink to="/product" className={currentPathCondition("/product")}>
+            Product
+          </NavLink>
+          <NavLink to="/contact" className={currentPathCondition("/contact")}>
+            Contact Us
+          </NavLink>
+        </div>
+        <div className="flex gap-4">
+          <button className="text-2xl" onClick={handleOnClickProfile}>
+            <MdOutlineSearch />
+          </button>
+          <button className="text-2xl" onClick={handleOnClickProfile}>
+            <MdOutlinePerson />
+          </button>
+          <button className="text-2xl" onClick={handleOnClickProfile}>
+            <MdOutlineShoppingBag  />
+          </button>
+        </div>
+      </div>
+    </>
+  );
+};
 
-export default NavBar
+export default NavBar;
